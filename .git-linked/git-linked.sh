c7787837336baf7494b7813e8f6ef8073ed05fe5
Nathan Myles <myles.n.a@gmail.com>
2018-04-19 00:03:05 -0300

    add hurt sound on failure

--- .git-linked/git-linked.sh
+++ .git-linked/git-linked.sh
@@ -4,44 +4,51 @@ git () {
 
     # do the git command
     command git "$@"
+    local command_status=$?
 
-    local audioFile
+    local audio_file
 
     # git status or git log
     if [[ $@ == status* || $@ == log* ]] ;
     then
-        audioFile=~/.git-linked/zelda-secret.mp3
+        audio_file=~/.git-linked/zelda-secret.mp3
     fi
 
     # git commit
     if [[ $@ == commit* ]] ;
     then
-        audioFile=~/.git-linked/zelda-small-item.mp3
+        audio_file=~/.git-linked/zelda-small-item.mp3
     fi
 
     # git push
     if [[ $@ == push* ]] ;
     then
-        audioFile=~/.git-linked/zelda-item.mp3
+        audio_file=~/.git-linked/zelda-item.mp3
     fi
 
     # git pull (or up)
     if [[ $@ == pull* || $@ == up* ]] ;
     then
-        audioFile=~/.git-linked/zelda-heart-container.mp3
+        audio_file=~/.git-linked/zelda-heart-container.mp3
     fi
 
     # git clone
     if [[ $@ == clone* ]] ;
     then
-        audioFile=~/.git-linked/zelda-spirit-orb.mp3
+        audio_file=~/.git-linked/zelda-spirit-orb.mp3
+    fi
+
+    # command failed 
+    if [ $command_status -ne 0 ] ;
+    then
+        audio_file=~/.git-linked/zelda-link-hurt.mp3
     fi
 
     # play sound based on OS
     if [[ "$OSTYPE" == "darwin"* ]] ;
     then 
-        (Afplay ${audioFile} &)
+        (Afplay ${audioi_file} &)
     else 
-        (ffplay -autoexit -nodisp -nostats -loglevel 0 ${audioFile} &)
+        (ffplay -autoexit -nodisp -nostats -loglevel 0 ${audio_file} &)
     fi
 }
