69de16b86f8d7320038af99bf8df30b2b8a4920b
Nathan Myles <myles.n.a@gmail.com>
2018-04-18 23:31:41 -0300

    Update to support ubuntu

--- .git-linked/git-linked.sh
+++ .git-linked/git-linked.sh
@@ -4,35 +4,44 @@ git () {
 
     # do the git command
     command git "$@"
-    
+
+    local audioFile
+
     # git status or git log
     if [[ $@ == status* || $@ == log* ]] ;
     then
-        (Afplay ~/.git-linked/zelda-secret.mp3 &)
+        audioFile=~/.git-linked/zelda-secret.mp3
     fi
 
     # git commit
     if [[ $@ == commit* ]] ;
     then
-        (Afplay ~/.git-linked/zelda-small-item.mp3 &)
+        audioFile=~/.git-linked/zelda-small-item.mp3
     fi
 
     # git push
     if [[ $@ == push* ]] ;
     then
-        (Afplay ~/.git-linked/zelda-item.mp3 &)
+        audioFile=~/.git-linked/zelda-item.mp3
     fi
 
     # git pull (or up)
     if [[ $@ == pull* || $@ == up* ]] ;
     then
-        (Afplay ~/.git-linked/zelda-heart-container.mp3 &)
+        audioFile=~/.git-linked/zelda-heart-container.mp3
     fi
 
     # git clone
     if [[ $@ == clone* ]] ;
     then
-        (Afplay ~/.git-linked/zelda-spirit-orb.mp3 &)
+        audioFile=~/.git-linked/zelda-spirit-orb.mp3
     fi
 
+    # play sound based on OS
+    if [[ "$OSTYPE" == "darwin"* ]] ;
+    then 
+        (Afplay ${audioFile} &)
+    else 
+        (ffplay -autoexit -nodisp -nostats -loglevel 0 ${audioFile} &)
+    fi
 }
